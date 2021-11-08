0901ed4426b09ad02141c5b502d0f5e4c19cbd0f
Ron Toland <code@ron.toland.io>
2021-11-07 16:02:13 -0800

    bring in tools.cli

--- project.clj
+++ project.clj
@@ -5,8 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [clojure.java-time "0.3.3"]
-                 [doric "0.9.0"]]
+                 [doric "0.9.0"]
+                 [org.clojure/tools.cli "1.0.206"]]
   :main ^:skip-aot tiny-records.core
-  :target-path "target/%s"
+  :target-path "target"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
