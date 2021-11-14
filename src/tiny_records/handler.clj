d430bbd41d83ee3f2c248c65b0fa7ee2fc43108b
Ron Toland <code@ron.toland.io>
2021-11-14 13:54:08 -0800

    add doc strings for handler functions

--- src/tiny_records/handler.clj
+++ src/tiny_records/handler.clj
@@ -5,10 +5,12 @@
             [ring.util.response :as resp]))
 
 (defn get-status
+  "Handler for checking status of the server."
   [req]
   (resp/response {:message "Server running"}))
 
 (defn wrap-json-body
+  "Middleware to coerce responses to json."
   [handler]
   (fn [request]
     (let [response (handler request)]
@@ -17,9 +19,11 @@
           (update :body json/generate-string)))))
 
 (defroutes app-routes
+  "Defines all the routes for the rest api."
   (GET "/status" [] get-status)
   (route/not-found (resp/not-found {:message "Not Found"})))
 
 (def app
+  "This is the entrypoint for the web server."
   (-> app-routes
       wrap-json-body))
