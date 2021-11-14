5bce4b4945107041540fadba30e1f744e8550dd7
Ron Toland <code@ron.toland.io>
2021-11-14 11:26:05 -0800

    add dependencies and handler ns for basic web server

--- project.clj
+++ project.clj
@@ -5,9 +5,18 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [clojure.java-time "0.3.3"]
+                 [compojure "1.6.1"]
                  [doric "0.9.0"]
-                 [org.clojure/tools.cli "1.0.206"]]
+                 [org.clojure/tools.cli "1.0.206"]
+                 [ring/ring-core "1.9.4"]
+                 [ring/ring-devel "1.9.4"]
+                 [ring/ring-jetty-adapter "1.9.4"]]
   :main ^:skip-aot tiny-records.core
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler tiny-records.handler/app}
   :target-path "target"
-  :profiles {:uberjar {:aot :all
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]]}
+             :uberjar {:aot :all
+                       :main tiny-records.core
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ src/tiny_records/handler.clj
@@ -0,0 +1,10 @@
+(ns tiny-records.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> app-routes))
