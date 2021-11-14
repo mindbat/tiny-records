75c06e6d9eeb96d7bcfc11d1ac529823aa0b2664
Ron Toland <code@ron.toland.io>
2021-11-14 13:50:01 -0800

    test basic routing: status, 404 for random reqs

--- test/tiny_records/test/handler.clj
+++ test/tiny_records/test/handler.clj
@@ -11,9 +11,21 @@
        ~@body
        (finally (.stop server#)))))
 
-(deftest t-get-status
-  (with-server handler/app {:port 3000}
-    (let [response (http/get "http://localhost:3000/status")]
-      (is (= 200 (:status response)))
-      (is (= "Server running"
-             (:message (json/parse-string (:body response) true)))))))
+(deftest t-basic-routes
+  (testing "should have a status endpoint with json response"
+    (with-server handler/app {:port 3000}
+      (let [response (http/get "http://localhost:3000/status")]
+        (is (= 200 (:status response)))
+        (is (= "application/json"
+               (get-in response [:headers "Content-Type"])))
+        (is (= "Server running"
+               (:message (json/parse-string (:body response) true)))))))
+  (testing "should 404 for random requests"
+    (with-server handler/app {:port 3000}
+      (let [response (http/get "http://localhost:3000/pinfeathers"
+                               {:throw-exceptions false})]
+        (is (= 404 (:status response)))
+        (is (= "application/json"
+               (get-in response [:headers "Content-Type"])))
+        (is (= "Not Found"
+               (:message (json/parse-string (:body response) true))))))))
