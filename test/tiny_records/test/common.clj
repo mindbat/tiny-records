2e50d008471864e991b1398074d5ac7f6aa69093
Ron Toland <code@ron.toland.io>
2021-11-15 07:04:14 -0800

    use a with-server-fixture to speed up tests
    less time spent waiting for web server to spin up and down

--- test/tiny_records/test/common.clj
+++ test/tiny_records/test/common.clj
@@ -1,5 +1,7 @@
 (ns tiny-records.test.common
-  (:require [tiny-records.record :as rec]))
+  (:require [tiny-records.handler :as handler]
+            [tiny-records.record :as rec]
+            [ring.adapter.jetty :as ring-jetty]))
 
 (defn reset-records!
   "Reset the current records list."
@@ -12,3 +14,15 @@
   (reset-records!)
   (when f
     (f)))
+
+(defmacro with-server [app options & body]
+  `(let [server# (ring-jetty/run-jetty ~app ~(assoc options :join? false))]
+     (try
+       ~@body
+       (finally (.stop server#)))))
+
+(defn with-server-fixture
+  [f]
+  (when f
+    (with-server handler/app {:port 3000}
+      (f))))
--- test/tiny_records/test/handler.clj
+++ test/tiny_records/test/handler.clj
@@ -9,154 +9,140 @@
             [tiny-records.test.common :as common]))
 
 (use-fixtures :each
-  common/reset-records-fixture)
-
-(defmacro with-server [app options & body]
-  `(let [server# (ring-jetty/run-jetty ~app ~(assoc options :join? false))]
-     (try
-       ~@body
-       (finally (.stop server#)))))
+  common/reset-records-fixture
+  common/with-server-fixture)
 
 (deftest t-basic-routes
   (testing "should have a status endpoint with json response"
-    (with-server handler/app {:port 3000}
-      (let [response (http/get "http://localhost:3000/status")]
-        (is (= 200 (:status response)))
-        (is (= "application/json"
-               (get-in response [:headers "Content-Type"])))
-        (is (= "Server running"
-               (:message (json/parse-string (:body response) true)))))))
+    (let [response (http/get "http://localhost:3000/status")]
+      (is (= 200 (:status response)))
+      (is (= "application/json"
+             (get-in response [:headers "Content-Type"])))
+      (is (= "Server running"
+             (:message (json/parse-string (:body response) true))))))
   (testing "should 404 for random requests"
-    (with-server handler/app {:port 3000}
-      (let [response (http/get "http://localhost:3000/pinfeathers"
-                               {:throw-exceptions false})]
-        (is (= 404 (:status response)))
-        (is (= "application/json"
-               (get-in response [:headers "Content-Type"])))
-        (is (= "Not Found"
-               (:message (json/parse-string (:body response) true))))))))
+    (let [response (http/get "http://localhost:3000/pinfeathers"
+                             {:throw-exceptions false})]
+      (is (= 404 (:status response)))
+      (is (= "application/json"
+             (get-in response [:headers "Content-Type"])))
+      (is (= "Not Found"
+             (:message (json/parse-string (:body response) true)))))))
 
 (deftest t-create-record
   (testing "should accept pipe-delimited records"
     (common/reset-records!)
-    (with-server handler/app {:port 3000}
-      (let [test-record "the-owl|archimedes|wise@owl.com|brown|287-04-06"
-            test-body (json/generate-string {:record-line test-record})
-            response (http/post "http://localhost:3000/records"
-                                {:content-type :json
-                                 :body test-body})
-            response-body (json/parse-string (:body response)
-                                             true)]
-        (is (= 201
-               (:status response)))
-        (is (= "the-owl"
-               (:last-name response-body)))
-        (is (= 1 (count @rec/current-records)))
-        (is (= "the-owl"
-               (:last-name (first @rec/current-records)))))))
+    (let [test-record "the-owl|archimedes|wise@owl.com|brown|287-04-06"
+          test-body (json/generate-string {:record-line test-record})
+          response (http/post "http://localhost:3000/records"
+                              {:content-type :json
+                               :body test-body})
+          response-body (json/parse-string (:body response)
+                                           true)]
+      (is (= 201
+             (:status response)))
+      (is (= "the-owl"
+             (:last-name response-body)))
+      (is (= 1 (count @rec/current-records)))
+      (is (= "the-owl"
+             (:last-name (first @rec/current-records))))))
   (testing "should accept comma-delimited records"
     (common/reset-records!)
-    (with-server handler/app {:port 3000}
-      (let [test-record "the-owl,archimedes,wise@owl.com,brown,287-04-06"
-            test-body (json/generate-string {:record-line test-record})
-            response (http/post "http://localhost:3000/records"
-                                {:content-type :json
-                                 :body test-body})
-            response-body (json/parse-string (:body response)
-                                             true)]
-        (is (= 201
-               (:status response)))
-        (is (= "the-owl"
-               (:last-name response-body)))
-        (is (= 1 (count @rec/current-records)))
-        (is (= "the-owl"
-               (:last-name (first @rec/current-records)))))))
+    (let [test-record "the-owl,archimedes,wise@owl.com,brown,287-04-06"
+          test-body (json/generate-string {:record-line test-record})
+          response (http/post "http://localhost:3000/records"
+                              {:content-type :json
+                               :body test-body})
+          response-body (json/parse-string (:body response)
+                                           true)]
+      (is (= 201
+             (:status response)))
+      (is (= "the-owl"
+             (:last-name response-body)))
+      (is (= 1 (count @rec/current-records)))
+      (is (= "the-owl"
+             (:last-name (first @rec/current-records))))))
   (testing "should accept space-delimited records"
     (common/reset-records!)
-    (with-server handler/app {:port 3000}
-      (let [test-record "the-owl archimedes wise@owl.com brown 287-04-06"
-            test-body (json/generate-string {:record-line test-record})
-            response (http/post "http://localhost:3000/records"
-                                {:content-type :json
-                                 :body test-body})
-            response-body (json/parse-string (:body response)
-                                             true)]
-        (is (= 201
-               (:status response)))
-        (is (= "the-owl"
-               (:last-name response-body)))
-        (is (= 1 (count @rec/current-records)))
-        (is (= "the-owl"
-               (:last-name (first @rec/current-records)))))))
+    (let [test-record "the-owl archimedes wise@owl.com brown 287-04-06"
+          test-body (json/generate-string {:record-line test-record})
+          response (http/post "http://localhost:3000/records"
+                              {:content-type :json
+                               :body test-body})
+          response-body (json/parse-string (:body response)
+                                           true)]
+      (is (= 201
+             (:status response)))
+      (is (= "the-owl"
+             (:last-name response-body)))
+      (is (= 1 (count @rec/current-records)))
+      (is (= "the-owl"
+             (:last-name (first @rec/current-records))))))
   (testing "should reject non-json bodies"
-    (with-server handler/app {:port 3000}
-      (let [response (http/post "http://localhost:3000/records"
-                                {:throw-exceptions false
-                                 :body "this is not json"})]
-        (is (= 400
-               (:status response)))
-        (is (= "Must send a valid record in the body!"
-               (:message (json/parse-string (:body response) true)))))))
+    (let [response (http/post "http://localhost:3000/records"
+                              {:throw-exceptions false
+                               :body "this is not json"})]
+      (is (= 400
+             (:status response)))
+      (is (= "Must send a valid record in the body!"
+             (:message (json/parse-string (:body response) true))))))
   (testing "should reject empty body"
-    (with-server handler/app {:port 3000}
-      (let [test-body (json/generate-string {:record-line ""})
-            response (http/post "http://localhost:3000/records"
-                                {:throw-exceptions false
-                                 :content-type :json
-                                 :body test-body})]
-        (is (= 400
-               (:status response)))
-        (is (= "Must send a valid record in the body!"
-               (:message (json/parse-string (:body response) true)))))))
+    (let [test-body (json/generate-string {:record-line ""})
+          response (http/post "http://localhost:3000/records"
+                              {:throw-exceptions false
+                               :content-type :json
+                               :body test-body})]
+      (is (= 400
+             (:status response)))
+      (is (= "Must send a valid record in the body!"
+             (:message (json/parse-string (:body response) true))))))
   (testing "should reject malformed records"
-    (with-server handler/app {:port 3000}
-      (let [test-record "this-is-a-bad-record"
-            test-body (json/generate-string {:record-line test-record})
-            response (http/post "http://localhost:3000/records"
-                                {:throw-exceptions false
-                                 :content-type :json
-                                 :body test-body})]
-        (is (= 400
-               (:status response)))
-        (is (= "Must send a valid record in the body!"
-               (:message (json/parse-string (:body response) true))))))))
+    (let [test-record "this-is-a-bad-record"
+          test-body (json/generate-string {:record-line test-record})
+          response (http/post "http://localhost:3000/records"
+                              {:throw-exceptions false
+                               :content-type :json
+                               :body test-body})]
+      (is (= 400
+             (:status response)))
+      (is (= "Must send a valid record in the body!"
+             (:message (json/parse-string (:body response) true)))))))
 
 (deftest t-get-current-records
-  (with-server handler/app {:port 3000}
-    ;; load up our current records
-    (with-open [rdr (io/reader "test/sample-pipe-delimited.txt")]
-      (doseq [line (line-seq rdr)]
-        (http/post "http://localhost:3000/records"
-                   {:throw-exceptions false
-                    :content-type :json
-                    :body (json/generate-string {:record-line line})})))
-    (testing "should be able to fetch by color"
-      (let [response (http/get "http://localhost:3000/records/color")
-            response-body (json/parse-string (:body response)
-                                             true)]
-        (is (= 200
-               (:status response)))
-        (is (= 6
-               (count (:records response-body))))
-        (is (= ["mordred" "wizard" "the-owl" "gawain" "of-the-lake" "wart"]
-               (map :last-name (:records response-body))))))
-    (testing "should be able to fetch by birthdate"
-      (let [response (http/get "http://localhost:3000/records/birthdate")
-            response-body (json/parse-string (:body response)
-                                             true)]
-        (is (= 200
-               (:status response)))
-        (is (= 6
-               (count (:records response-body))))
-        (is (= ["the-owl" "of-the-lake" "gawain" "wart" "mordred" "wizard"]
-               (map :last-name (:records response-body))))))
-    (testing "should be able to fetch by last name"
-      (let [response (http/get "http://localhost:3000/records/name")
-            response-body (json/parse-string (:body response)
-                                             true)]
-        (is (= 200
-               (:status response)))
-        (is (= 6
-               (count (:records response-body))))
-        (is (= ["gawain" "mordred" "of-the-lake" "the-owl" "wart" "wizard"]
-               (map :last-name (:records response-body))))))))
+  ;; load up our current records
+  (with-open [rdr (io/reader "test/sample-pipe-delimited.txt")]
+    (doseq [line (line-seq rdr)]
+      (http/post "http://localhost:3000/records"
+                 {:throw-exceptions false
+                  :content-type :json
+                  :body (json/generate-string {:record-line line})})))
+  (testing "should be able to fetch by color"
+    (let [response (http/get "http://localhost:3000/records/color")
+          response-body (json/parse-string (:body response)
+                                           true)]
+      (is (= 200
+             (:status response)))
+      (is (= 6
+             (count (:records response-body))))
+      (is (= ["mordred" "wizard" "the-owl" "gawain" "of-the-lake" "wart"]
+             (map :last-name (:records response-body))))))
+  (testing "should be able to fetch by birthdate"
+    (let [response (http/get "http://localhost:3000/records/birthdate")
+          response-body (json/parse-string (:body response)
+                                           true)]
+      (is (= 200
+             (:status response)))
+      (is (= 6
+             (count (:records response-body))))
+      (is (= ["the-owl" "of-the-lake" "gawain" "wart" "mordred" "wizard"]
+             (map :last-name (:records response-body))))))
+  (testing "should be able to fetch by last name"
+    (let [response (http/get "http://localhost:3000/records/name")
+          response-body (json/parse-string (:body response)
+                                           true)]
+      (is (= 200
+             (:status response)))
+      (is (= 6
+             (count (:records response-body))))
+      (is (= ["gawain" "mordred" "of-the-lake" "the-owl" "wart" "wizard"]
+             (map :last-name (:records response-body)))))))
